8cb585c444088e4b037deecc55ed37f64958e61a
Asaduzzaman04 <asadchy152@gmail.com>
2024-08-26 23:40:24 +0600

    portfolio-created

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,17 +3,31 @@ module.exports = {
   content: ["*"],
   theme: {
     extend: {},
-    colors :{
-      "background-dark" : "#150a3a",
-      "background-light" : "#d8dcff",
-      "font-primary" : "#d8dcff",
-      "font-secondery" : "#150a3a",
-      "button-bg-color" : "#d8dcff",
-      "button-color" : "#150a3a",
+    fontFamily:{
+      "poppins" : "Poppins, system-ui"
     },
-    fontFamily : {
-      "poppins" : ["Poppins", "sans-serif"]
+    borderRadius : {
+      "border" : " 50% 50% 50% 50% / 50% 50% 50% 50%",
+      "full" : "9999px",
+      "medium" : "10px 10px 10px 10px"
     },
+    boxShadow:{
+      "base" : '0px 0px 4px #dfdfdf',
+      "sm" : '3px 3px 2px #636e72',
+      "lg" : '0px 0px 10px #636e72',
+    },
+    backgroundImage:{
+      "gridiant-btn" : " linear-gradient(to right, #3498db, #4498d2, #5198ca, #5c98c1, #6798b9)",
+    },
+    animation:{
+      "round" : 'spin 6s linear infinite',
+      "bounce1" : 'bounce 5s infinite ',
+      "bounce2" : 'bounce 5s infinite 1s',
+      "bounce3" : 'bounce 5s infinite 2s',
+      "bounce4" : 'bounce 5s infinite 3s',
+      "bounce5" : 'bounce 5s infinite 4s ',
+      "bounce6" : 'bounce 5s infinite 5s',
+    }
   },
   plugins: [],
 }
